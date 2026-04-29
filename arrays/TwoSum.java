621842e700453436f11699068ff77564510c6a2f
Likitha <likithasuguna2006@gmail.com>
2026-04-29 23:18:50 +0530

    Day 1: Two Sum

---
+++ arrays/TwoSum.java
@@ -0,0 +1,22 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Approach: Brute Force
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+package arrays;
+class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
