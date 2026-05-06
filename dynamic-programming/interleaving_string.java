aec433abc11cd0ecb9eaaed05347533112e7d1ef
Likitha <likithasuguna2006@gmail.com>
2026-05-06 23:00:24 +0530

    Day 8: Interleaving String

---
+++ dynamic-programming/interleaving_string.java
@@ -0,0 +1,47 @@
+/*
+Problem: Interleaving String
+Platform: LeetCode
+Approach: Dynamic Programming (2D DP)
+
+Time Complexity: O(n * m)
+Space Complexity: O(n * m)
+*/
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        // Length mismatch impossible
+        if (n + m != s3.length()) return false;
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        // Base case
+        dp[0][0] = true;
+
+        // Fill first column (only s1)
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+        }
+
+        // Fill first row (only s2)
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j - 1] && (s2.charAt(j - 1) == s3.charAt(j - 1));
+        }
+
+        // Fill rest of DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j= 1; j <= m; j++) {
+
+                char c = s3.charAt(i + j - 1);
+
+                // Take from s1 OR s2
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == c) ||
+                           (dp[i][j - 1] && s2.charAt(j - 1) == c);
+            }
+        }
+        return dp[n][m];
+    }
+}
