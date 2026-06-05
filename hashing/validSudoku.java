e3a9fba2fb10c2640f86256be34ca27b4be85322
Likitha <likithasuguna2006@gmail.com>
2026-06-05 13:37:16 +0530

    Day 38: Valid Sudoku

---
+++ hashing/validSudoku.java
@@ -0,0 +1,40 @@
+/*
+Problem: Valid Sudoku
+Platform: LeetCode 36
+Approach: HashSet
+
+Time Complexity:  O(1)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num == '.') continue;
+
+                String row = num + " in row " + i;
+                String col = num + " in col " + j;
+                String box = num + " in box " + (i / 3) + "-" + (j / 3);
+
+                if (set.contains(row) ||
+                    set.contains(col) ||
+                    set.contains(box))
+                    return false;
+
+                set.add(row);
+                set.add(col);
+                set.add(box);
+            }
+        }
+
+        return true;
+    }
+}
