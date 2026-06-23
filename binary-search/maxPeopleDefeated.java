997b8290b715016edb7edd2704e86b337cb73aee
Likitha <likithasuguna2006@gmail.com>
2026-06-23 07:01:15 +0530

    Day 56:  Maximum Number of People That Can Be Defeated

--- binary-search/maxPeopleDefeated.java
+++ binary-search/maxPeopleDefeated.java
@@ -9,18 +9,18 @@ Space Complexity: O(1)
 
 class Solution {
     int maxPeopleDefeated(int p) {
-        long long left = 0, right = 1500;
+        long left = 0, right = 1500;
 
         while (left < right) {
-            long long mid = left + (right - left + 1) / 2;
-            long long sum = mid * (mid + 1) * (2 * mid + 1) / 6;
+            long mid = left + (right - left + 1) / 2;
+            long sum = mid * (mid + 1) * (2 * mid + 1) / 6;
 
-            if (sum <= (long long)p)
+            if (sum <= (long) p)
                 left = mid;
             else
                 right = mid - 1;
         }
 
-        return (int)left;
+        return (int) left;
     }
-};
+}
