88f96b4f6be747ecddd0dfebcfcef209428f913c
Likitha <likithasuguna2006@gmail.com>
2026-07-09 09:28:59 +0530

    Day 70: Path Existence Queries in a Graph 1

---
+++ graphs/pathExistenceQueries.java
@@ -0,0 +1,36 @@
+/*
+
+Problem: Path Existence Queries in a Graph 1
+Platform: Leetcode 
+Approach: Connected Components
+
+Time Complexity: O(n + q)
+Space Complexity: O(n)
+
+*/
+
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+
+        int[] comp = new int[n];
+        int id = 0;
+        comp[0] = id;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                id++;
+            }
+            comp[i] = id;
+        }
+
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            ans[i] = (comp[u] == comp[v]);
+        }
+
+        return ans;
+    }
+}
