f0f4356d192552fc9eb50c2ef6b26717fec47747
Likitha <likithasuguna2006@gmail.com>
2026-07-09 09:18:48 +0530

    Count Pairs Divisible By K

---
+++ hashing/countKdivPairs.java
@@ -0,0 +1,22 @@
+/* 
+
+Problem: Count Pairs Divisible By K 
+Approach: Hashing 
+
+Time Complexity: O(n)
+Space Complexity: O(k)
+
+*/
+class Solution {
+    public int countKdivPairs(int[] arr, int k) {
+        int[] freq = new int[k];
+        int count = 0;
+        for (int num : arr) {
+            int rem = num % k;
+            int comp = (k - rem) % k;
+            count += freq[comp];
+            freq[rem]++;
+        }
+        return count;
+    }
+}
